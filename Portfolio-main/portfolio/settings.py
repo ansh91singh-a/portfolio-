6d5382f83ba2ecdcb17f32c82cd970fac20a64ea
your name <ansh91singh02@gmail.com>
2026-08-15 16:48:04 +0530

    fix django vercel deployment

--- Portfolio-main/portfolio/settings.py
+++ Portfolio-main/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#xyi$*x*ak2xk_ge2885rxii#!q%-@+7yb@dvstg^#a^pmirzh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
